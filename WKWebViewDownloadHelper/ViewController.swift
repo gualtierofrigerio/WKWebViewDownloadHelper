baa31f5a8e4c1d8bd84ad5d5a883853a72d067c5
Gualtiero Frigerio <gualtiero.frigerio@gmail.com>
2020-07-03 20:51:47 +0200

    first commit

---
+++ WKWebViewDownloadHelper/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  WkWebViewTest
+//
+//  Created by Gualtiero Frigerio on 03/07/2020.
+//  Copyright © 2020 Gualtiero Frigerio. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class ViewController: UIViewController {
+    var webView:WKWebView!
+    var helper:WKWebviewDownloadHelper!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let webView = WKWebView(frame: self.view.frame)
+        self.view.addSubview(webView)
+        webView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        helper = WKWebviewDownloadHelper(webView: webView, mimeTypes: ["ms-excel"], delegate: self)
+        let request = URLRequest(url: URL(string: "https://testurl.it")!)
+        webView.load(request)
+        self.webView = webView
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .close, target: nil, action: nil)
+    }
+}
+
+extension ViewController: WKWebViewDownloadHelperDelegate {
+    func fileDownloadedAtURL(url: URL) {
+        DispatchQueue.main.async {
+            let activityVC = UIActivityViewController(activityItems: [url], applicationActivities: nil)
+            activityVC.popoverPresentationController?.sourceView = self.view
+            activityVC.popoverPresentationController?.barButtonItem = self.navigationItem.rightBarButtonItem
+            self.present(activityVC, animated: true, completion: nil)
+        }
+    }
+}
---
+++ WKWebViewDownloadHelper/WKWebViewDownloadHelper.swift
@@ -0,0 +1,109 @@
+//
+//  WKWebViewDownloadHelper.swift
+//  WkWebViewTest
+//
+//  Created by Gualtiero Frigerio on 03/07/2020.
+//  Copyright © 2020 Gualtiero Frigerio. All rights reserved.
+//
+
+import Foundation
+import WebKit
+
+protocol WKWebViewDownloadHelperDelegate {
+    func fileDownloadedAtURL(url:URL)
+}
+
+class WKWebviewDownloadHelper:NSObject {
+    
+    var webView:WKWebView
+    var mimeTypes:[String]
+    var delegate:WKWebViewDownloadHelperDelegate
+    
+    init(webView:WKWebView, mimeTypes:[String], delegate:WKWebViewDownloadHelperDelegate) {
+        self.webView = webView
+        self.mimeTypes = mimeTypes
+        self.delegate = delegate
+        super.init()
+        webView.navigationDelegate = self
+    }
+    
+    private func downloadData(fromURL url:URL,
+                              fileName:String,
+                              completion:@escaping (Bool, URL?) -> Void) {
+        webView.configuration.websiteDataStore.httpCookieStore.getAllCookies() { cookies in
+            let session = URLSession.shared
+            session.configuration.httpCookieStorage?.setCookies(cookies, for: url, mainDocumentURL: nil)
+            let task = session.downloadTask(with: url) { localURL, urlResponse, error in
+                if let localURL = localURL {
+                    let destinationURL = self.moveDownloadedFile(url: localURL, fileName: fileName)
+                    completion(true, destinationURL)
+                }
+                else {
+                    completion(false, nil)
+                }
+            }
+
+            task.resume()
+        }
+    }
+    
+    private func getFileNameFromResponse(_ response:URLResponse) -> String {
+        if let httpResponse = response as? HTTPURLResponse {
+            let headers = httpResponse.allHeaderFields
+            if let disposition = headers["Content-Disposition"] as? String {
+                let components = disposition.components(separatedBy: " ")
+                if components.count > 1 {
+                    let innerComponents = components[1].components(separatedBy: "=")
+                    if innerComponents.count > 1 {
+                        if innerComponents[0].contains("filename") {
+                            return innerComponents[1]
+                        }
+                    }
+                }
+            }
+        }
+        return "default"
+    }
+    
+    private func isMimeTypeConfigured(_ mimeType:String) -> Bool {
+        for type in self.mimeTypes {
+            if mimeType.contains(type) {
+                return true
+            }
+        }
+        return false
+    }
+    
+    private func moveDownloadedFile(url:URL, fileName:String) -> URL {
+        let tempDir = NSTemporaryDirectory()
+        let destinationPath = tempDir + fileName
+        let destinationURL = URL(fileURLWithPath: destinationPath)
+        try? FileManager.default.removeItem(at: destinationURL)
+        try? FileManager.default.moveItem(at: url, to: destinationURL)
+        return destinationURL
+    }
+}
+
+extension WKWebviewDownloadHelper: WKNavigationDelegate {
+    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+        decisionHandler(.allow)
+    }
+    
+    func webView(_ webView: WKWebView, decidePolicyFor navigationResponse: WKNavigationResponse, decisionHandler: @escaping (WKNavigationResponsePolicy) -> Void) {
+        if let mimeType = navigationResponse.response.mimeType {
+            if isMimeTypeConfigured(mimeType) {
+                if let url = navigationResponse.response.url {
+                    let fileName = getFileNameFromResponse(navigationResponse.response)
+                    downloadData(fromURL: url, fileName: fileName) { success, destinationURL in
+                        if success, let destinationURL = destinationURL {
+                            self.delegate.fileDownloadedAtURL(url: destinationURL)
+                        }
+                    }
+                    decisionHandler(.cancel)
+                    return
+                }
+            }
+        }
+        decisionHandler(.allow)
+    }
+}
